5696a6b83735c453b99b64d7dc2fd68a21a08609
Neha-Mule <150320983+Neha-Mule@user.noreply.com>
2025-02-17 13:35:30 +0530

    modified user added new field email

--- src/main/java/com/example/git/module/User.java
+++ src/main/java/com/example/git/module/User.java
@@ -4,6 +4,15 @@ public class User {
     private int id;
     private String name;
     private String pass;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public int getId() {
         return id;
